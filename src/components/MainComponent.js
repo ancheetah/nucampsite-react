c0f41bfc59a05c48409a0da751ab0fe6d6492d0d
AJ <ancheetah@users.noreply.github.com>
2020-12-02 00:26:26 -0500

    adding to single page app

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,17 +3,24 @@ import Home from './HomeComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 // import { Navbar, NavbarBrand } from 'reactstrap';
 import Directory from './DirectoryComponent';
-import CampsiteInfo from './CampsiteInfoComponent';
+// import CampsiteInfo from './CampsiteInfoComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Contact from './ContactComponent';
 import { CAMPSITES } from '../shared/campsites';
+import { COMMENTS } from '../shared/comments';
+import { PARTNERS } from '../shared/partners';
+import { PROMOTIONS } from '../shared/promotions';
 
 class Main extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            campsites: CAMPSITES
+            campsites: CAMPSITES,
+            comments: COMMENTS,
+            partners: PARTNERS,
+            promotions: PROMOTIONS
         };
     }
 
@@ -25,7 +32,11 @@ class Main extends Component {
 
         const HomePage = () => {
             return (
-                <Home />
+                <Home
+                    campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
+                    promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
+                    partner={this.state.partners.filter(partner => partner.featured)[0]}
+                />
             );
         };
 
@@ -35,6 +46,7 @@ class Main extends Component {
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                    <Route exact path='/contactus' component={Contact} />
                     <Redirect to='/home' />
                 </Switch>
                 <Footer />
