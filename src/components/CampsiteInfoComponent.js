b84cb7135c3398c0fdf8828d24309d1534594d12
AJ <ancheetah@users.noreply.github.com>
2020-12-02 02:04:52 -0500

    breadcrumbs

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,13 +1,6 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
-
-// class CampsiteInfo extends Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         // selectedCampsite: null
-    //     };
-    // }
+import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 function RenderCampsite({campsite}) {
         return (
@@ -15,15 +8,14 @@ function RenderCampsite({campsite}) {
                 <Card>
                      <CardImg top src={campsite.image} alt={campsite.name} />
                      <CardBody>
-                         <CardTitle>{campsite.name}</CardTitle>
                          <CardText>{campsite.description}</CardText>
                      </CardBody>
-                 </Card>
+                </Card>
             </div>
         );
     }
 
-    //Note: see render() in directorycomponent.js. Should we render components in the render() method instead?
+//Note: see render() in directorycomponent.js. Should we render components in the render() method instead?
 function RenderComments({comments}) {
     if (comments) {
         return (
@@ -42,6 +34,16 @@ function CampsiteInfo(props) {
     if (props.campsite) {
         return (
             <div class="container">
+                <div className="row">
+                    <div className="col">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <h2>{props.campsite.name}</h2>
+                        <hr />
+                    </div>
+                </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
                     <RenderComments comments={props.comments} />
