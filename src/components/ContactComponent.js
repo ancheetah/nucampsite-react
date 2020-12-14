6fcb9d864d5654de74e1bc90a730a1126e9c5820
AJ <ancheetah@users.noreply.github.com>
2020-12-14 01:14:03 -0500

    form validation

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Breadcrumb, BreadcrumbItem, Button, Form, FormGroup, Label, Input, Col } from 'reactstrap';
+import { Breadcrumb, BreadcrumbItem, Button, Form, FormGroup, Label, Input, Col, FormFeedback } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 class Contact extends Component {
@@ -7,21 +7,82 @@ class Contact extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {
+        this.state = {  //set inital states here
             firstName: '',
             lastName: '',
             phoneNum: '',
             email: '',
             agree: false, //Does user want to be contacted?
             contactType: 'By Phone',
-            feedback: ''
+            feedback: '',
+            touched: {  
+                //nested object that keeps track of whether or not user has entered anything in the form field
+                //if the field hasnt been touched then no need to validate that field yet
+                //use the blur event handler to know if a user has touched the field yet
+                firstName: false,
+                lastName: false,
+                phoneNum: false,
+                email: false
+            }
         };
 
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        //handleBlur() is bound using an arrow function instead
     }
 
-    //Handle changes in form elements
+    validate(firstName, lastName, phoneNum, email) {    //called at beginning of render()
+
+        const errors = {    //holds error messages
+            firstName: '',
+            lastName: '',
+            phoneNum: '',
+            email: ''
+        };
+
+        if (this.state.touched.firstName) {
+            if (firstName.length < 2) {
+                errors.firstName = 'First name must be at least 2 characters.';
+            } else if (firstName.length > 15) {
+                errors.firstName = 'First name must be 15 or less characters.';
+            }
+        }
+
+        if (this.state.touched.lastName) {
+            if (lastName.length < 2) {
+                errors.lastName = 'Last name must be at least 2 characters.';
+            } else if (lastName.length > 15) {
+                errors.lastName = 'Last name must be 15 or less characters.';
+            }
+        }
+
+        const reg = /^\d+$/;
+        if (this.state.touched.phoneNum && !reg.test(phoneNum)) {
+            errors.phoneNum = 'The phone number should contain only numbers.';
+        }
+
+        if (this.state.touched.email && !email.includes('@')) {
+            errors.email = 'Email should contain a @';
+        }
+
+        return errors;
+    }
+
+    //This event handler is fired when a user enters an input field then leaves it (loses focus)
+    //When hadle blur is triggered it will change the field's "touched" state to true
+    //Add this event handler to every form field (i.e. <input>) you want to track
+    //The "field" parameter accepts a string with the name of the field (i.e. value of "name" attribute of the <input> element)
+    //Binding using arrow function syntax. Why? We're not passing an event object here but rather a string
+    handleBlur = (field) => {
+        return () => { 
+            this.setState({
+                touched: {...this.state.touched, [field]: true} //changes only one of the properties in "touched"
+            });
+        }
+    }
+
+
+    //Update the state of the form field 
     handleInputChange(event) {
         const target = event.target;    //the <input> element
         const name = target.name;       //the value of <input>'s "name" attribute    
@@ -45,6 +106,10 @@ class Contact extends Component {
     }
 
     render() {
+
+        //Initialize the errors array (will be blank at this point because fields have not yet been touched)
+        const errors = this.validate(this.state.firstName, this.state.lastName, this.state.phoneNum, this.state.email);
+
         return (
             <div className="container">
                 <div className="row">
@@ -80,13 +145,20 @@ class Contact extends Component {
                     </div>
                     <div className="col-md-10">
                         <Form onSubmit={this.handleSubmit}>
-                            <FormGroup row>
+                        <FormGroup row>
                                 <Label htmlFor="firstName" md={2}>First Name</Label>
-                                <Col md={10}> {/* reactstrap equivalent of "col-md-10" */}
+                                <Col md={10}>
                                     <Input type="text" id="firstName" name="firstName"
                                         placeholder="First Name"
                                         value={this.state.firstName}
-                                        onChange={this.handleInputChange} />
+                                        invalid={errors.firstName}  //a boolean bootstrap attribute
+                                        onBlur={this.handleBlur("firstName")}   //tracks if user has touched form field
+                                        onChange={this.handleInputChange} //Update the state of the form field
+                                    />
+                                    {/* Form Feedback is a reactstrap component used when valid/invalid are set in an 
+                                    <input> element. Displays invalid feedback message in red and valid feedback in green below form field.
+                                    Only displays if invalid === true in this case */}
+                                    <FormFeedback>{errors.firstName}</FormFeedback>
                                 </Col>
                             </FormGroup>
                             <FormGroup row>
@@ -95,8 +167,11 @@ class Contact extends Component {
                                     <Input type="text" id="lastName" name="lastName"
                                         placeholder="Last Name"
                                         value={this.state.lastName}
+                                        invalid={errors.lastName}
+                                        onBlur={this.handleBlur("lastName")}
                                         onChange={this.handleInputChange} />
-                                </Col>                        
+                                    <FormFeedback>{errors.lastName}</FormFeedback>
+                                </Col>
                             </FormGroup>
                             <FormGroup row>
                                 <Label htmlFor="phoneNum" md={2}>Phone</Label>
@@ -104,7 +179,10 @@ class Contact extends Component {
                                     <Input type="tel" id="phoneNum" name="phoneNum"
                                         placeholder="Phone number"
                                         value={this.state.phoneNum}
+                                        invalid={errors.phoneNum}
+                                        onBlur={this.handleBlur("phoneNum")}
                                         onChange={this.handleInputChange} />
+                                    <FormFeedback>{errors.phoneNum}</FormFeedback>
                                 </Col>
                             </FormGroup>
                             <FormGroup row>
@@ -113,7 +191,10 @@ class Contact extends Component {
                                     <Input type="email" id="email" name="email"
                                         placeholder="Email"
                                         value={this.state.email}
+                                        invalid={errors.email}
+                                        onBlur={this.handleBlur("email")}
                                         onChange={this.handleInputChange} />
+                                    <FormFeedback>{errors.email}</FormFeedback>
                                 </Col>
                             </FormGroup>
                             <FormGroup row>
