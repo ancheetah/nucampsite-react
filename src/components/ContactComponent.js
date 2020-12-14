6f46b28137cd378d3cfc1e6fe31cd37fcb81230a
AJ <ancheetah@users.noreply.github.com>
2020-12-13 23:24:35 -0500

    added a react controlled form

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,37 +1,166 @@
-import React from 'react';
-import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, { Component } from 'react';
+import { Breadcrumb, BreadcrumbItem, Button, Form, FormGroup, Label, Input, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
-function Contact(props) {
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="col">
-                    <Breadcrumb>
-                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
-                        <BreadcrumbItem active>Contact Us</BreadcrumbItem>
-                    </Breadcrumb>
-                    <h2>Contact Us</h2>
-                    <hr />
-                </div>
-            </div>
+class Contact extends Component {
 
-            <div className="row row-content align-items-center">
-                <div className="col-sm-4">
-                    <h5>Our Address</h5>
-                    <address>
-                        1 Nucamp Way<br />
-                        Seattle, WA 98001<br />
-                        U.S.A.
-                    </address>
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            firstName: '',
+            lastName: '',
+            phoneNum: '',
+            email: '',
+            agree: false, //Does user want to be contacted?
+            contactType: 'By Phone',
+            feedback: ''
+        };
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    //Handle changes in form elements
+    handleInputChange(event) {
+        const target = event.target;    //the <input> element
+        const name = target.name;       //the value of <input>'s "name" attribute    
+
+        //Is <input> a checkbox? If yes, then return <input>'s checked attribute (boolean) to get the user's choice
+        //If no then use <input>'s value attribute to get the text that the user entered
+        const value = target.type === 'checkbox' ? target.checked : target.value;   //target.type is the value of <input>'s "type" attribute (i.e. "button", "checkbox", "color", "month", etc.)
+    
+        //Create an object with a computed property name using the value of <input>'s name attribute
+        //Update the state with the user's input -> sets firstName, lastName, phoneNum, email, agree, contactType, or feedback to the user's input value
+        this.setState({
+            [name]: value
+        });
+    }
+
+    //Log current state to the console
+    handleSubmit(event) {
+        console.log('Current state is: ' + JSON.stringify(this.state));
+        alert('Current state is: ' + JSON.stringify(this.state));
+        event.preventDefault(); //Prevents page from refreshing after this event
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>Contact Us</BreadcrumbItem>
+                        </Breadcrumb>
+                        <h2>Contact Us</h2>
+                        <hr />
+                    </div>
                 </div>
-                <div className="col">
-                    <a role="button" className="btn btn-link" href="tel:+12065551234"><i className="fa fa-phone" /> 1-206-555-1234</a><br />
-                    <a role="button" className="btn btn-link" href="mailto:fakeemail@fakeemail.co"><i className="fa fa-envelope-o" /> campsites@nucamp.co</a>
+    
+                <div className="row row-content align-items-center">
+                    <div className="col-sm-4">
+                        <h5>Our Address</h5>
+                        <address>
+                            1 Nucamp Way<br />
+                            Seattle, WA 98001<br />
+                            U.S.A.
+                        </address>
+                    </div>
+                    <div className="col">
+                        <a role="button" className="btn btn-link" href="tel:+12065551234"><i className="fa fa-phone" /> 1-206-555-1234</a><br />
+                        <a role="button" className="btn btn-link" href="mailto:fakeemail@fakeemail.co"><i className="fa fa-envelope-o" /> campsites@nucamp.co</a>
+                    </div>
                 </div>
+
+                <div className="row row-content">
+                    <div className="col-12">
+                        <h2>Send us your Feedback</h2>
+                        <hr />
+                    </div>
+                    <div className="col-md-10">
+                        <Form onSubmit={this.handleSubmit}>
+                            <FormGroup row>
+                                <Label htmlFor="firstName" md={2}>First Name</Label>
+                                <Col md={10}> {/* reactstrap equivalent of "col-md-10" */}
+                                    <Input type="text" id="firstName" name="firstName"
+                                        placeholder="First Name"
+                                        value={this.state.firstName}
+                                        onChange={this.handleInputChange} />
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label htmlFor="lastName" md={2}>Last Name</Label>
+                                <Col md={10}>
+                                    <Input type="text" id="lastName" name="lastName"
+                                        placeholder="Last Name"
+                                        value={this.state.lastName}
+                                        onChange={this.handleInputChange} />
+                                </Col>                        
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label htmlFor="phoneNum" md={2}>Phone</Label>
+                                <Col md={10}>
+                                    <Input type="tel" id="phoneNum" name="phoneNum"
+                                        placeholder="Phone number"
+                                        value={this.state.phoneNum}
+                                        onChange={this.handleInputChange} />
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label htmlFor="email" md={2}>Email</Label>
+                                <Col md={10}>
+                                    <Input type="email" id="email" name="email"
+                                        placeholder="Email"
+                                        value={this.state.email}
+                                        onChange={this.handleInputChange} />
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Col md={{size: 4, offset: 2}}>
+                                    <FormGroup check>
+                                        <Label check>
+                                            <Input type="checkbox"
+                                                name="agree"
+                                                checked={this.state.agree}
+                                                onChange={this.handleInputChange} /> {' '}
+                                            <strong>May we contact you?</strong>
+                                        </Label>
+                                    </FormGroup>
+                                </Col>
+                                <Col md={4}>
+                                    <Input type="select" name="contactType"
+                                            value={this.state.contactType}
+                                            onChange={this.handleInputChange}>
+                                        <option>By Phone</option>
+                                        <option>By Email</option>
+                                    </Input>
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label htmlFor="feedback" md={2}>Your Feedback</Label>
+                                <Col md={10}>
+                                    <Input type="textarea" id="feedback" name="feedback"
+                                        rows="12"
+                                        value={this.state.feedback}
+                                        onChange={this.handleInputChange}></Input>
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Col md={{size: 10, offset: 2}}>
+                                    <Button type="submit" color="primary">
+                                        Send Feedback
+                                    </Button>
+                                </Col>
+                            </FormGroup>
+                        </Form>
+                    </div>
+                </div>
+
             </div>
-        </div>
-    );
+        );
+    }
 }
+
 
 export default Contact;
