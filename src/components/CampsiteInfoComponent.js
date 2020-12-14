6df9a98c82f5094f18fc49d206a83f2b921e672f
AJ <ancheetah@users.noreply.github.com>
2020-12-14 10:02:29 -0500

    Redux form validation

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -4,6 +4,10 @@ import { Card, CardImg, CardText, CardBody, Button, Breadcrumb, BreadcrumbItem,
         Modal, ModalHeader, ModalBody, Label } from 'reactstrap';
 import {Control, LocalForm, Errors} from 'react-redux-form';
 
+//Input Validation Methods
+const maxLength = len => val => !val || (val.length <= len);
+const minLength = len => val => val && (val.length >= len);
+
 function RenderCampsite({campsite}) {
         return (
             <div className="col-md-5 m-1">
@@ -17,13 +21,22 @@ function RenderCampsite({campsite}) {
         );
     }
 
-//Note: see render() in directorycomponent.js. Should we render components in the render() method instead?
 function RenderComments({comments}) {
     if (comments) {
         return (
             <div className="col-md-5 m-1">
             <h4>Comments</h4>
-            {comments.map( comment => <p key={comment.id}>{comment.text}<br/>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p> )}
+            {comments.map( comment => {
+                    return (
+                        <p key={comment.id}>
+                            {comment.text}<br/>-- {comment.author}, {new Intl.DateTimeFormat(
+                                'en-US', { year: 'numeric', month: 'short', day: '2-digit'})
+                                .format(new Date(Date.parse(comment.date)))}
+                        </p> 
+                        );
+                })
+            }
+            
             <br />
             <CommentForm/>
             </div>
@@ -32,6 +45,34 @@ function RenderComments({comments}) {
 
     //else
     return (<div></div>);
+}
+
+function CampsiteInfo(props) {
+
+    if (props.campsite) {
+        return (
+            <div class="container">
+                <div className="row">
+                    <div className="col">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <h2>{props.campsite.name}</h2>
+                        <hr />
+                    </div>
+                </div>
+                <div className="row">
+                    <RenderCampsite campsite={props.campsite} />
+                    <RenderComments comments={props.comments} />
+                </div>
+                
+            </div>
+        );
+    }
+    //else
+    return (<div></div>);
+
 }
 
 class CommentForm extends Component {
@@ -61,13 +102,22 @@ class CommentForm extends Component {
 
     render() {
         return (
-            <div>
+            <React.Fragment> {/* Use React.Fragment instead of wrapping multiple elements in a <div> to avoid adding an extra, unecessary node */}
+
                 <Button outline onClick={this.toggleModal}><i className="fa fa-lg fa-pencil"/> Submit Comment</Button>
-    
+
+                {/* Open the comment form modal when "Submit Comment" is clicked */}
                 <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}> {/* isOpen and toggle are built-in attributes of <Modal> in reactstrap */}
-                    <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader> {/* adding toggle here allows user to close modal when it is open */}
+                    
+                    <ModalHeader toggle={this.toggleModal}> {/* adding toggle here allows user to close modal when it is open */}
+                        Submit Comment
+                    </ModalHeader> 
+
                     <ModalBody>
-                        <LocalForm onSubmit={values => this.handleSubmit(values)}>
+                        
+                        <LocalForm onSubmit={values => this.handleSubmit(values)}> {/* Redux forms handle submit events here */}
+
+                            {/* Select rating 1-5 */}
                             <div className="form-group">
                                 <Label htmlFor="rating">Rating</Label>
                                 <Control.select model=".rating" id="rating" name="rating"
@@ -81,54 +131,49 @@ class CommentForm extends Component {
                                     <option value="5">5</option>
                                 </Control.select>
                             </div>
+
+                            {/* Author Name */}
                             <div className="form-group">
                                 <Label htmlFor="author">Your Name</Label>
                                 <Control.text model=".author" id="author" name="author"
                                             placeholder="Your Name"
                                             className="form-control"
+                                            validators = {{
+                                                minLength: minLength(2),
+                                                maxLength: maxLength(15)
+                                            }}
+                                />
+                                <Errors 
+                                    model=".author"
+                                    className="text-danger"
+                                    show="touched"
+                                    component="div"
+                                    messages={{
+                                        minLength: "Must be at least 2 characters",
+                                        maxLength: "Must be 15 characters or less"
+                                    }}
                                 />
                             </div>
+
+                            {/* Comment Textarea */}
                             <div className="form-group">
                                 <Label htmlFor="text">Comment</Label>
                                 <Control.textarea model=".text" id="text" name="text"
                                             className="form-control"
                                 />
                             </div>
+
                             <Button type="submit" value="submit" color="primary">Submit</Button>
+
                         </LocalForm>
+
                     </ModalBody>
+
                 </Modal>
-            </div>
+
+            </React.Fragment>
         );
     }
-}
-
-function CampsiteInfo(props) {
-
-    if (props.campsite) {
-        return (
-            <div class="container">
-                <div className="row">
-                    <div className="col">
-                        <Breadcrumb>
-                            <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
-                            <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
-                        </Breadcrumb>
-                        <h2>{props.campsite.name}</h2>
-                        <hr />
-                    </div>
-                </div>
-                <div className="row">
-                    <RenderCampsite campsite={props.campsite} />
-                    <RenderComments comments={props.comments} />
-                </div>
-                
-            </div>
-        );
-    }
-    //else
-    return (<div></div>);
-
 }
 
 
